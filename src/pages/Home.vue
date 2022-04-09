699a8a7d0425beeda450f816048ae448f6fbae3a
felex kemboi <felokemboi10@gmail.com>
2022-04-09 20:35:05 +0300

    style top images

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,25 @@
 <template>
   <q-page class="flex flex-start">
-    <div class="row">
+    <q-img src="../assets/top.png" style="height:220px;z-index: 1;"/>
+    <q-img src="../assets/toptop.png" style="width: 30%;height:200px;position:absolute;top: 130px;left: 35px;z-index: 2;"/>
+    <div class="q-pa-md" style="margin-top:120px;padding-left:40px;">
+      <div class="row" style="color:#76cfbf">
+        <strong>Kunsthal Rotterdam - demo</strong>
+      </div>
+
+      <div class="row q-gutter-sm"  style="margin-top:10px;" v-if="records.collection_picture">
+        <div>
+          <img :src="getPhoto(records.collection_picture.content_url)" style="height: 50px; max-width: 50px;border-radius: 12px;">
+        </div>
+        <div>
+          {{ records.name }}
+        </div>
+      </div>
+
+    </div>
+    <div class="row" v-if="records" style="padding-left:5px;">
       <q-list v-for="(memo,index) in records.memos_per_collection" :key="index">
-        <div class="q-pa-md col-xs-12 col-sm-6 col-md-4" style="min-width:320px;padding-left:10px;">
+        <div class="q-pa-md col-xs-12 col-sm-6 col-md-4" style="min-width:320px;">
           <q-card class="bg-grey-1" style="overflow-wrap: break-word;inline-size: 430px;">
             <q-item-section class="q-pt-md q-pl-md">
               <q-item-label caption>
@@ -26,12 +43,7 @@
               </q-item-label>
             </q-item-section>
             <q-item class="q-pl-sm">
-              <!-- <div v-for="(photo,index) in memo.memo.memo_photos" :key="index">
-                <q-item-label>{{ photo.content_url }}</q-item-label>
-                <img :src="getPhoto(photo.content_url)" style="height: 170px; max-width: 300px">
-              </div> -->
-              <div v-if="memo.memo.memo_photos[0].content_url">
-                <!-- <q-item-label>{{ memo.memo.memo_photos[0].content_url }}</q-item-label> -->
+              <div v-if="memo.memo.memo_photos.length > 0">
                 <img :src="getPhoto(memo.memo.memo_photos[0].content_url)" style="height: 170px; max-width: 300px">
               </div>
               <div v-else>
@@ -57,7 +69,7 @@ export default {
   },
   methods:{
     getPhoto(url) {
-      return `https://api.staging.quodari.com/${url}`;
+      return  `https://api.staging.quodari.com/${url}`;
     },
     computeTime(rawDate){
       return moment(rawDate).format("dddd -  MMMM Do YYYY")
